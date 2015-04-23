0940d8102ab28955cc48f9dd06b924a562d8ba2d
Andy-P <apemmelaar@gmail.com>
2015-04-23 18:41:45 +0900

    Fix of uncheck commits

--- example/example.jl
+++ example/example.jl
@@ -1,6 +1,5 @@
 using RecurrentNN
 reload("RecurrentNN.jl")
-
 # # global settings
 const generator = "rnn" # can be 'rnn' or 'lstm'
 const hiddensizes = [20,20] # list of sizes of hidden layers
@@ -8,20 +7,15 @@ const lettersize = 5 # size of letter embeddings
 
 # optimization
 const regc = 0.000001 # L2 regularization strength
-const learning_rate = 0.01 # learning rate
+const learning_rate = 0.0001 # learning rate
 const clipval = 5.20 # clip gradients at this value
-
-type TextModel
-    wil::RecurrentNN.NNMatrix  # character input to char-index layer
-    model::RecurrentNN.Model
-end
 
 function initVocab(inpath::String)
 
     f = open(inpath,"r")
     str = readall(inpath)
-    vocab = sort(setdiff(unique(str),['\n'])) # unique characters in data
-    sents = split(str,'\n') # array of sentences
+    vocab = sort(setdiff(unique(str),['\r','\n'])) # unique characters in data
+    sents = split(str,"\r\n") # array of sentences
     inputsize = length(vocab) + 1 # 1 additional token (zero) in used for beginning and end tokens
     outputsize = length(vocab) + 1
     epochsize = length(sents) # nmber of sentence in sample
@@ -38,8 +32,8 @@ end
 function initModel(inputsize::Int, lettersize::Int, hiddensizes::Array{Int,1},outputsize::Int)
     wil = RecurrentNN.randNNMat(inputsize,lettersize,.008)
     nn = RecurrentNN.RNN(lettersize,hiddensizes,outputsize)
-    println((typeof(wil),typeof(nn)))
-    return TextModel(wil,nn)
+#     println((typeof(wil),typeof(nn)))
+    return wil, nn
 end
 
 #########################################
@@ -53,8 +47,12 @@ sents, vocab, letterToIndex, indexToLetter, inputsize, outputsize, epochsize =
     initVocab(joinpath(dirname(@__FILE__),"samples.txt"))
 
 # init the rnn/lstm
-model = initModel(inputsize, lettersize, hiddensizes, outputsize)
+wil, model = initModel(inputsize, lettersize, hiddensizes, outputsize)
 
+tickiter = 0
+
+pplcurve = Array(FloatingPoint,0) # track perplexity
+pplgraph = Dict{Int,FloatingPoint} # track perplexity
 
 #########################################
 #             run the model             #
@@ -64,58 +62,108 @@ function forwardIndex()
 
 end
 
-function predictsentence(model::Model, sent::String)
+function predictsentence(model::RecurrentNN.Model, sent::String)
 
 
 end
 
-
-function costfunc(model::Model, sent::String)
+function costfunc(model::RecurrentNN.Model, wil::RecurrentNN.NNMatrix, sent::String)
 
     # takes a model and a sentence and
     # calculates the loss. Also returns the Graph
     # object which can be used to do backprop
-
     n = length(sent)
-    g = Graph()
+    g = RecurrentNN.Graph()
     log2ppl = 0.0
     cost = 0.0
-    prevhd = Array(NNMatrix,0) # final hidden layer of the recurrent model after each forward step
-    prevout = NNMatrix(outputsize,1) # output of the recurrent model after each forward step
+    prevhd = Array(RecurrentNN.NNMatrix,0) # final hidden layer of the recurrent model after each forward step
+    prevout = RecurrentNN.NNMatrix(outputsize,1) # output of the recurrent model after each forward step
     for i= 0:length(sent)
-        c = i == 0? 0 : sent[i]
-        println((i,c))
 
-    # start and end tokens are zeros
-    ix_source = i == 0 ? 0 : letterToIndex[sent[i]] # first step: start with START token
-    ix_target = i == n ? 0 : letterToIndex[sent[i+1]] # last step: end with END token
+#         println((i,n))
+        # start and end tokens are zeros
+        ix_source = i == 0 ? 0 : letterToIndex[sent[i]] # first step: start with START token
+        ix_target = i == n ? 0 : letterToIndex[sent[i+1]] # last step: end with END token
 
-    lh = forwardIndex(g, model, ix_source, prev);
-    prevhd, prevout = lh
+#         println((0, i,ix_source,ix_target))
+        x = RecurrentNN.rowpluck(g, wil, ix_source+1)
+        prevhd, prevout = RecurrentNN.forwardprop(g, model, x, prevhd, prevout)
 
-    # set gradients into logprobabilities
-    logprobs = lh.o # interpret output as logprobs
-    probs = R.softmax(logprobs) # compute the softmax probabilities
+#         println((1, i,ix_source,ix_target))
+        # set gradients into logprobabilities
+        logprobs = prevout # interpret output as logprobs
+        probs = RecurrentNN.softmax(logprobs) # compute the softmax probabilities
 
-    log2ppl += -Math.log2(probs.w[ix_target]); // accumulate base 2 log prob and do smoothing
-    cost += -Math.log(probs.w[ix_target]);
+#         println((2, i,ix_source,ix_target))
+        log2ppl += -log2(probs.w[ix_target+1]) # accumulate base 2 log prob and do smoothing
+        cost += -log(probs.w[ix_target+1])
 
-    # write gradients into log probabilities
-    logprobs.dw = probs.w;
-    logprobs.dw[ix_target] -= 1
-    }
-    var ppl = Math.pow(2, log2ppl / (n - 1));
-    return {'G':G, 'ppl':ppl, 'cost':cost};
-
+        # write gradients into log probabilities
+        logprobs.dw = probs.w;
+        logprobs.dw[ix_target+1] -= 1
+    end
+    ppl = (log2ppl / (n - 1))^2
+    return g, ppl, cost
 end
 
-function costfunc(model::Model, sent::String)
+function tick(model::RecurrentNN.Model, wil::RecurrentNN.NNMatrix, sents::Array, solver::RecurrentNN.Solver, tickiter::Int, pplcurve::Array{FloatingPoint,1})
+
+    # sample sentence fromd data
+    sentix = rand(1:length(sents))
 
 
+    t1 = time_ns() # log start timestamp
+
+    # evaluate cost function on a sentence
+    g, ppl, cost = costfunc(model,wil, sent)
+
+    # use built up graph to compute backprop (set .dw fields in mats)
+    for i = length(g.backprop):-1:1  g.backprop[i]() end
+
+    # perform param update ( learning_rate, regc, clipval are global constants)
+    solverstats = RecurrentNN.step(solver, model, learning_rate, regc, clipval)
+
+    tm = (time_ns()-t1)/1e9
+
+    push!(pplcurve, ppl) #keep track of perplexity
+
+    tickiter += 1;
+    if tickiter % 50 == 0
+    # draw samples
+    println("#samples $tickiter")
+#     for(var q=0;q<5;q++) {
+#       var pred = predictSentence(model, true, sample_softmax_temperature);
+#       var pred_div = '<div class="apred">'+pred+'</div>'
+#       $('#samples').append(pred_div);
+#     }
+    end
+
+    if tickiter % 10 == 0
+#     // draw argmax prediction
+#     $('#argmax').html('');
+#     var pred = predictSentence(model, false);
+#     var pred_div = '<div class="apred">'+pred+'</div>'
+#     $('#argmax').append(pred_div);
+
+    # keep track of perplexity
+#     $('#epoch').text('epoch: ' + (tick_iter/epoch_size).toFixed(2));
+#     $('#ppl').text('perplexity: ' + cost_struct.ppl.toFixed(2));
+#     $('#ticktime').text('forw/bwd time per example: ' + tick_time.toFixed(1) + 'ms');
+
+        if tickiter % 100 == 0
+            pplmedian = median(pplcurve)
+            println((typeof(tickiter),typeof(pplmedian)))
+            pplgraph[tickiter] = pplmedian
+            pplcurve = Array(FloatingPoint,0)
+        end
+    end
+
+    return model, wil, solver, tickiter
 end
 
-sent = sents[20]
-for i= 0:length(sent)
-    c = i == 0? 0 : sent[i]
-    println((i,c))
+tic()
+interations =  1000
+for i = 1:interations
+    model, wil, solver, tickiter  = tick(model, wil, sents, solver, tickiter, pplcurve)
 end
+toc()
--- src/rnn.jl
+++ src/rnn.jl
@@ -3,8 +3,8 @@ type RNNLayer # a single layer of a multi-layer RNN
     whh::NNMatrix # hidden to hidden weights & gradients
     bhh::NNMatrix # bias of hidden to hidden
     function RNNLayer(prevsize::Int, hiddensize::Int, std::FloatingPoint)
-        wxh = randNNMat(prevsize, hiddensize, std)
-        whh = randNNMat(prevsize, hiddensize, std)
+        wxh = randNNMat(hiddensize, prevsize, std)
+        whh = randNNMat(hiddensize, hiddensize, std)
         wbh = NNMatrix(hiddensize, 1, zeros(hiddensize), zeros(hiddensize))
         new(wxh, whh, wbh)
     end
@@ -14,7 +14,7 @@ type RNN <: Model
     hdlayers::Array{RNNLayer,1} # holds variable number of hidden layers
     whd::NNMatrix # hidden to decoder weights & gradients
     bd::NNMatrix  # bias of hidden to decoder layer
-    hiddensizes::{Int,1}
+    hiddensizes::Array{Int,1}
     function RNN(inputsize::Int, hiddensizes::Array{Int,1}, outputsize::Int, std::FloatingPoint=0.08)
         hdlayers = Array(RNNLayer, length(hiddensizes))
         for d in 1:length(hiddensizes)
@@ -23,9 +23,27 @@ type RNN <: Model
             hdlayers[d] = layer
         end
         whd = randNNMat(outputsize, hiddensizes[end], std)
-        bd = NNMatrix(outputsize, 1, zeros(outputsize), zeros(outputsize))
+        bd = NNMatrix(outputsize, 1, zeros(outputsize,1), zeros(outputsize,1))
+#         println("bh.w size($(size(bd.w)))")
         new(hdlayers, whd, bd, hiddensizes)
     end
+end
+
+# this function does not exist in Andrej Karpathy's version
+# due to difference between JavaScript. it has been added
+# to allow the solver to collect and adjust all of the
+# matrices' weights without knowing the structure of any given model
+function collectNNMat(model::RNN)
+    modelNNMats = Array(NNMatrix,0)
+    for d in 1:length(model.hiddensizes)
+        layer = model.hdlayers[d]
+        push!(modelNNMats, layer.wxh) # input (X or prev layer) to hidden weights & gradients
+        push!(modelNNMats, layer.whh) # hidden to hidden weights & gradients
+        push!(modelNNMats, layer.bhh) # bias of hidden to hidden
+    end
+    push!(modelNNMats, model.whd) # bias of hidden to hidden
+    push!(modelNNMats, model.bd) # bias of hidden to hidden
+    return modelNNMats
 end
 
 function forwardprop(g::Graph, model::RNN, x, prevhd, prevout)
@@ -37,7 +55,7 @@ function forwardprop(g::Graph, model::RNN, x, prevhd, prevout)
     # prev is a struct containing hidden activations from last step
 
     hiddenprevs = Array(NNMatrix,0)
-    if size(prevs,1) == 0
+    if length(prevhd) == 0
         for hdsize in model.hiddensizes
             push!(hiddenprevs, NNMatrix(hdsize,1))
         end
@@ -46,25 +64,28 @@ function forwardprop(g::Graph, model::RNN, x, prevhd, prevout)
     end
 
     hidden = Array(NNMatrix,0)
-    for d in 0:length(hiddensizes) # for each hidden layer
+    for d in 1:length(model.hiddensizes) # for each hidden layer
 
-        input = d == 0 ? x : hidden[d]
+        input = d == 1 ? x : hidden[d-1]
         hdprev = hiddenprevs[d]
         wxh = model.hdlayers[d].wxh
         whh = model.hdlayers[d].whh
         bhh = model.hdlayers[d].bhh
 
+#         println((d,size(wxh.w),(size(input.w))))
+
         h0 = mul(g, wxh, input)
         h1 = mul(g, whh, hdprev)
-        hidden_d = relu(add(g, add(g, h0, h1), bhh))
+#         println((typeof(h0),typeof(h1),typeof(bhh)))
+        hidden_d = relu(g, add(g, add(g, h0, h1), bhh))
 
         push!(hidden,hidden_d)
     end
 
     # one decoder to outputs at end
+#     println((size(model.whd.w), size(hidden[end].w), size(model.bd.w)))
     output = add(g, mul(g, model.whd, hidden[end]),model.bd)
 
     # return cell memory, hidden representation and output
     return hidden, output
 end
-
