5fc18d30be31a2aa1ec4d8894306cbb4c4816aee
Andy-P <apemmelaar@gmail.com>
2015-05-04 13:49:46 +0900

    Cleaning up some unnecessary comments

--- src/graph.jl
+++ src/graph.jl
@@ -9,7 +9,6 @@ function rowpluck(g::Graph, m::NNMatrix, ix::Int)
     # pluck a row of m and return it as a column vector
     out = NNMatrix(m.d, 1)
     out.w[:,1] = m.w[ix,:]'
-    # backprop function
     if g.doBackprop
         push!(g.backprop,
               function ()
@@ -22,7 +21,6 @@ end
 function tanh(g::Graph, m::NNMatrix)
     out = NNMatrix(m.n, m.d)
     out.w = tanh(m.w)
-    # backprop function
     if g.doBackprop
         push!(g.backprop,
               function ()
@@ -38,7 +36,6 @@ function sigmoid(g::Graph, m::NNMatrix)
     out = NNMatrix(m.n, m.d,
             [1.0 / (1.0 + exp(-m.w[i,j])) for i in 1:m.n, j in m.d],
             zeros(m.n, m.d))
-    # backprop function
     if g.doBackprop
         push!(g.backprop,
               function ()
@@ -55,12 +52,11 @@ function relu(g::Graph, m::NNMatrix)
     @inbounds for j = 1:m.d, i = 1:m.n
         out.w[i,j] = m.w[i,j] < 0. ? 0. : m.w[i,j]
     end
-    # backprop function
     if g.doBackprop
         push!(g.backprop,
           function ()
               @inbounds for j = 1:m.d, i = 1:m.n
-                  m.dw[i,j] +=  m.w[i,j] < 0. ? 0 : out.dw[i,j]
+                  m.dw[i,j] +=  m.w[i,j] < 0. ? 0. : out.dw[i,j]
               end
           end )
     end
@@ -69,23 +65,12 @@ end
 
 function mul(g::Graph, m1::NNMatrix, m2::NNMatrix)
     out = NNMatrix(m1.n, m2.d, m1.w * m2.w, zeros(m1.n, m2.d))
-
-    # backprop function
     if g.doBackprop
         push!(g.backprop,
             function ()
-                #  m1.dw : m1n x m1d 
-                # out.dw : m1n x m2d
-                #   m2.w : m1d x m2d
-                # m1.dw += out.dw * m2.w'
-
-                #  m2.dw : m1d x m2d 
-                # out.dw : m1n x m2d
-                #   m1.w : m1n x m1d
-                # mw.dw += m1.w' * out.dw
                 @inbounds for i = 1:m1.n, j = 1:m2.d
                     b = out.dw[i,j]
-                    for k = 1:m1.d # m1's num col
+                    for k = 1:m1.d
                         m1.dw[i,k] += m2.w[k,j] * b
 
                         m2.dw[k,j] += m1.w[i,k] * b
@@ -112,7 +97,6 @@ end
 
 function eltmul(g::Graph, m1::NNMatrix, m2::NNMatrix) # element-wise multiplication
     out = NNMatrix(m1.n, m2.d, m1.w .* m2.w, zeros(m1.n, m2.d))
-    # backprop function
     if g.doBackprop
         push!(g.backprop,
             function ()
